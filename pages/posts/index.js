98809419c0cf1ec07f9f8442eafc131feecd7047
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-04-16 09:16:12 +0700

    update: added posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,24 @@
-function AllPostsPage() {}
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    title: 'First posts',
+    date: '2020-05-20',
+    summary: 'This is summary',
+    slug: 'first-posts',
+    image: 'admin.png',
+  },
+  {
+    title: 'Secondary posts',
+    date: '2020-05-20',
+    summary: 'This is summary secondary',
+    slug: 'secondary-posts',
+    image: 'admin.png',
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
