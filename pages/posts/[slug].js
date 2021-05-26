ba2919a4b6394bf162985779b50cf1bec2a8dc00
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-05-27 06:34:02 +0700

    feat: integrate dynamic post pages & paths

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,6 +1,34 @@
 import PostContent from './../../components/posts/post-detail/post-content';
-function PostDetailPage() {
-  return <PostContent />;
+import { getPostData, getPostFiles } from '../../helpers/posts-util';
+function PostDetailPage(props) {
+  const { post } = props;
+  return <PostContent post={post} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostFiles();
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ''));
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug,
+      },
+    })),
+    fallback: 'blocking',
+  };
 }
 
 export default PostDetailPage;
