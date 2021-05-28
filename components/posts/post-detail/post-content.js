60affa147eda0071957d96113a6b97a1d3083368
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-05-28 15:56:55 +0700

    feat: added code for rendering images component from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,16 +1,49 @@
 import PostHeader from './post-header';
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import classes from './post-content.module.css';
 
 function PostContent(props) {
   const { post } = props;
+  const customRenderers = {
+    p(p) {
+      const { node } = p;
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{p.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader
         title={post.title}
         image={`/images/posts/${post.slug}/${post.image}`}
       />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
