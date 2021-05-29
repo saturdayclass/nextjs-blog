e6c902f1ba9aacc09d61b65997afb63d7e200fa3
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-05-29 17:15:07 +0700

    update: added metadata

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom';
-
 import classes from './notification.module.css';
 
 function Notification(props) {
@@ -17,11 +16,12 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById('notifications')
   );
 }
 
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,21 @@
+import Head from 'next/head';
 import AllPosts from '../../components/posts/all-posts';
 import { getAllPosts } from './../../helpers/posts-util';
 
 function AllPostsPage(props) {
   const { posts } = props;
-  return <AllPosts posts={posts} />;
+  return (
+    <>
+      <Head>
+        <title>Contact Me</title>
+        <meta
+          name="description"
+          content="A list of all programming related article or tutorials "
+        />
+      </Head>
+      <AllPosts posts={posts} />
+    </>
+  );
 }
 
 export default AllPostsPage;
